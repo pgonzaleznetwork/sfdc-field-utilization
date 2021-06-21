2872b289de676cfc9c10533fc3aa28ef4f198d44
PabloGithubDesktop <pablo@githubdesktop.com>
2021-06-21 17:26:53 +0100

    support for text area fields

--- examples/example.js
+++ examples/example.js
@@ -6,13 +6,13 @@ let getUsageMetrics = require('../src/index');
 * @apiVersion the version of the Salesforce API. If not specified or if it's lower than 49.0, we use 49.0 by default
 */
 let connection = {
-    token: '00D180000002dZB!AQYAQIiJPfIut1HvgXWY3vO3KK2EG8ZOG4BFDnk75CScrx_SNlDZw8HVnkLiyDwGXbuOxflvx.8ZebAVTdaYIQiXCzoAZ0bW',
-    url:'https://test--test.my.salesforce.com',
+    token: '00D180000002dZB!AQYAQAmLuy8nTlM_vWwmTjlkQ.xbHrqBcO7ahdiSw4gn0B_53urDnAjGt2Klk8jB2OyLeHnIbJufYCzHb9F3d6ewyp3WKlzS',
+    url:'https://guidewire--uat.my.salesforce.com',
     apiVersion:'49.0'
 };
 
 //let customField = 'Case.CS_Carbon_Copy_Email_1__c';
-let customField = 'Case.CS_Support_Group__c'
+let customField = 'Opportunity.Notes__c'
 
 async function example(){
     console.log('Getting usage metrics...');
